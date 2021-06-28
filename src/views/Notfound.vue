06a8c48981eee99635724b03b61644494d1ed9b6
random_fe <564030084@qq.com>
2021-06-28 16:30:35 +0800

    add 404

---
+++ src/views/Notfound.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>当前页面不存在
+        <router-link to="/">返回首页</router-link>
+    </div>
+</template>
+
+<script lang="ts">
+  export default {
+    name: 'Notfound'
+  };
+</script>
+
+<style lang="scss" scoped>
+
+</style>
